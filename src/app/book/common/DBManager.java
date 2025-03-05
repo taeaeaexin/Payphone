c46f0747993b2929f8c9393fb7f5ec2facb19bae
taeaeaexin <re_fuse@naver.com>
2025-03-05 16:19:44 +0900

    250305 1619

---
+++ src/app/book/common/DBManager.java
@@ -0,0 +1,46 @@
+package app.book.common;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+// Connection 객체를 생성, 전달
+// ResultSet, PreparedStatement, Connection 객체 종료 close()
+public class DBManager {
+
+	static String url = "jdbc:mysql://localhost:3306/madang";
+	static String user = "root";
+	static String pwd = "root";
+	
+	public static Connection getConnection() {
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection(url, user, pwd);
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return con;		
+	}
+
+	public static void releaseConnection(PreparedStatement pstmt, Connection con) {
+		try {
+			pstmt.close();
+			con.close();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void releaseConnection(ResultSet rs, PreparedStatement pstmt, Connection con) {
+		try {
+			if(rs != null) rs.close();
+			if(pstmt != null) pstmt.close();
+			if(con != null) con.close();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
